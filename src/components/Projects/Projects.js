2398791d4556817f26780f8a0dc8c33d7aaa2cbc
MCVNick <mcvnick@gmail.com>
2019-04-03 13:39:59 -0600

    styling, projects page, and skills page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
-import Particle from '../Particles/Particle'
+import Particle from './../Particles/Particle'
+import Footer from './../Footer/Footer'
+
+import nslVideo from './../../resources/nslVideo.mp4'
 
 import './Projects.scss'
 
 const Projects = () => {
     return (
         <main id='projects-parent-comp'>
+            <div className='projects'>
+                <h2>Projects</h2>
+                <article>
+                    <h1>NSL Personal Project</h1>
+                    <video src={nslVideo} controls='true'/>
+                    <a href='https://github.com/MCVNick/nsl-classifieds'>NSL GitHub</a>
+                    <a href='https://nsl.nicholasmcqueen.com/'>NSL Website</a>
+                    <p>This was a three week project that I was able to work on cloning 
+                        KSL's website. In this project I learned how to use CSS Grid, 
+                        SCSS, JavaScript to manipulate classes and cause tabs to close.
+                         I also worked on form validation and the back end. The back 
+                         end used massive. It also was able to control when the web 
+                         page loaded new content. This allowed for the API that I was 
+                         using to not hit it's daily limit.
+                    </p>
+                </article>
+            </div>
             <Particle />
+            <Footer />
         </main>
     )
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,13 +1,63 @@
 import React from 'react'
 
 import Particle from './../Particles/Particle'
+import Footer from './../Footer/Footer'
+
+import CSS3 from './../../resources/SkillIcons/CSS3.png'
+import Express from './../../resources/SkillIcons/Express.png'
+import HTML5 from './../../resources/SkillIcons/HTML5.png'
+import JavaScript from './../../resources/SkillIcons/JavaScript.png'
+import NodeJS from './../../resources/SkillIcons/NodeJS.png'
+import PostgreSQL from './../../resources/SkillIcons/PostgreSQL.png'
+import ReactIcon from './../../resources/SkillIcons/React.png'
+import Redux from './../../resources/SkillIcons/Redux.png'
 
 import './Skills.scss'
 
 const Skills = () => {
     return (
         <main id='skills-parent-comp'>
+            <div className='skills'>
+                <div className='skills-header'>
+                    <h1>Skills</h1>
+                </div>
+                <ul>
+                    <li>
+                        <a href='https://reactjs.org/docs/getting-started.html'><img src={ReactIcon} alt='React' /></a>
+                        <h1>React</h1>
+                    </li>
+                    <li>
+                        <a href='https://devdocs.io/javascript/'><img src={JavaScript} alt='JavaScript' /></a>
+                        <h1>JavaScript</h1>
+                    </li>
+                    <li>
+                        <a href='https://devdocs.io/html/'><img src={HTML5} alt='HTML5' /></a>
+                        <h1>HTML5</h1>
+                    </li>
+                    <li>
+                        <a href='https://devdocs.io/css/'><img src={CSS3} alt='CSS3' /></a>
+                        <h1>CSS3</h1>
+                    </li>
+                    <li>
+                        <a href='https://www.postgresql.org/docs/'><img src={PostgreSQL} alt='PostgreSQL' /></a>
+                        <h1>PostgreSQL</h1>
+                    </li>
+                    <li>
+                        <a href='https://nodejs.org/en/docs/'><img src={NodeJS} alt='Node.JS' /></a>
+                        <h1>Node.JS</h1>
+                    </li>
+                    <li>
+                        <a href='https://expressjs.com/en/api.html'><img src={Express} alt='Express' /></a>
+                        <h1>Express</h1>
+                    </li>
+                    <li>
+                        <a href='https://redux.js.org/'><img src={Redux} alt='Redux' /></a>
+                        <h1>Redux</h1>
+                    </li>
+                </ul>
+            </div>
             <Particle />
+            <Footer />
         </main>
     )
 }
