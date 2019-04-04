78b6552d2e651e77bce74d75b13a448ce1bd95fd
MCVNick <mcvnick@gmail.com>
2019-04-04 12:37:28 -0600

    more styling

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import axios from 'axios'
 
 import Particle from './../Particles/Particle'
@@ -10,7 +10,30 @@ import linkedInIcon from './../../resources/LinkedIn.png'
 
 import './ContactMe.scss'
 
+const handleSubmit = (event, buttonRef) => {
+    event.preventDefault();
+    const from = event.target[0].value
+    const name = event.target[1].value
+    const message = event.target[2].value
+    const obj = { from, name, message }
+    let button = buttonRef.current.classList
+
+    if (from && name && message) {
+        axios.post('/send', obj)
+        event.target[0].value = ''
+        event.target[1].value = ''
+        event.target[2].value = ''
+        button.add('pass')
+        setTimeout(function () { button.remove('pass') }, 500);
+    } else {
+        button.add('fail')
+        setTimeout(function () { button.remove('fail') }, 500);
+    }
+}
+
 const ContactMe = () => {
+    const buttonRef = createRef()
+
     return (
         <main id='contact-me-parent-comp'>
             <div className='contact-me'>
@@ -21,30 +44,15 @@ const ContactMe = () => {
                     <h1>nickmcqueen111@gmail.com</h1>
                     <h1>—</h1>
                     <h1>Tel: (801) 558-7870</h1>
-                    <a href='https://github.com/MCVNick' target="_blank" rel="noopener noreferrer"><img src={linkedInIcon} alt="LinkedIn"/></a>
-                    <a href='https://www.linkedin.com/in/n-mcqueen/' target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="GitHub"/></a>
-                    <a href='https://www.facebook.com/nick.mcqueen.585' target="_blank" rel="noopener noreferrer"><img src={facebookIcon} alt="Facebook"/></a>
+                    <a href='https://github.com/MCVNick' target="_blank" rel="noopener noreferrer"><img src={linkedInIcon} alt="LinkedIn" /></a>
+                    <a href='https://www.linkedin.com/in/n-mcqueen/' target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="GitHub" /></a>
+                    <a href='https://www.facebook.com/nick.mcqueen.585' target="_blank" rel="noopener noreferrer"><img src={facebookIcon} alt="Facebook" /></a>
                 </section>
-                <form onSubmit={(event) => {
-                        event.preventDefault();
-                        const from = event.target[0].value
-                        const name = event.target[1].value
-                        const message = event.target[2].value
-                        const obj = {from, name, message}
-
-                        if (from && name && message) {
-                            axios.post('/send', obj)
-                            event.target[0].value = ''
-                            event.target[1].value = ''
-                            event.target[2].value = ''
-                        } else {
-
-                        }
-                    }}>
-                    <input className='email-input' placeholder='Email'/>
-                    <input className='name-input' placeholder='Name'/>
-                    <textarea className='message-input' placeholder='Message'/>
-                    <button>Send</button>
+                <form onSubmit={(event) => { handleSubmit(event, buttonRef) }}>
+                    <input className='email-input' placeholder='Email' />
+                    <input className='name-input' placeholder='Name' />
+                    <textarea className='message-input' placeholder='Message' />
+                    <button ref={buttonRef}>Send</button>
                 </form>
             </div>
             <Particle />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,21 +4,23 @@ import { Link, withRouter } from 'react-router-dom'
 import './Navbar.scss'
 
 const Navbar = (props) => {
-    try {console.log(props)}catch(e){}
+    const { pathname } = props.location;
+    //this tests if the path in the url is exactly one of the following '/Home' or '/'
+    let testHome = /^(\/Home)|(\/$)/.test(pathname);
     return (
         props.location.pathname !== '/blank' &&
         <nav id='nav-parent-comp'>
             <div>
                 <ul>
-                    <li><Link to='/Home'>Home</Link></li>
-                    <li><Link to='/About-Me'>About Me</Link></li>
-                    <li><Link to='/Skills'>Skills</Link></li>
-                    <li><Link to='/Projects'>Projects</Link></li>
-                    <li><Link to='/Contact-Me'>Contact Me</Link></li>
+                    <li className={testHome ? 'selected' : null}><Link to='/Home'>Home</Link></li>
+                    <li className={props.location.pathname === '/About-Me' ? 'selected' : null}><Link to='/About-Me'>About Me</Link></li>
+                    <li className={props.location.pathname === '/Skills' ? 'selected' : null}><Link to='/Skills'>Skills</Link></li>
+                    <li className={props.location.pathname === '/Projects' ? 'selected' : null}><Link to='/Projects'>Projects</Link></li>
+                    <li className={props.location.pathname === '/Contact-Me' ? 'selected' : null}><Link to='/Contact-Me'>Contact Me</Link></li>
                 </ul>
                 <h1>Nick McQueen</h1>
             </div>
-        </nav>
+        </nav >
     )
 }
 
