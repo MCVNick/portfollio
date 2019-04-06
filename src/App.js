9c8c59d92f23c202262ee8d88291bd3d54e1d0fb
MCVNick <mcvnick@gmail.com>
2019-04-05 21:55:57 -0600

    partially responsive on home page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import routes from './routes'
 
 import './reset.scss';
 import './App.scss';
+import Particle from './components/Particles/Particle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <p className="not-supported">
+        This device is not supported.
+        A screen width of at least 220 pixels is required.
+        If you somehow have a device that is this small,
+        contact me and I will attempt to make my site support
+        the thing. Contact me at (801) 558-7870
+      </p>
+      <div id='not-supported-particles'><Particle/></div>
         <Router>
           <Navbar />
           {routes}
