addce65db551eaef5f13ac97339db4478992853a
MCVNick <mcvnick@gmail.com>
2019-04-05 22:12:52 -0600

    still working on responsive design on home page, navbar is tricky because of z-index

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,15 +25,6 @@ const Navbar = (props) => {
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
-            <div className='overlay-selector'>
-                <ol>
-                    <li className={testHome ? 'selected' : null}><Link to='/Home'>Home</Link></li>
-                    <li className={props.location.pathname === '/About-Me' ? 'selected' : null}><Link to='/About-Me'>About Me</Link></li>
-                    <li className={props.location.pathname === '/Skills' ? 'selected' : null}><Link to='/Skills'>Skills</Link></li>
-                    <li className={props.location.pathname === '/Projects' ? 'selected' : null}><Link to='/Projects'>Projects</Link></li>
-                    <li className={props.location.pathname === '/Contact-Me' ? 'selected' : null}><Link to='/Contact-Me'>Contact Me</Link></li>
-                </ol>
-            </div>
         </nav >
     )
 }
