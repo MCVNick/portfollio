23ef3734e8b1713e865fb95fa116139e5852378f
MCVNick <mcvnick@gmail.com>
2019-04-08 18:15:53 -0600

    ok now sitemap is working

--- server/index.js
+++ server/index.js
@@ -4,21 +4,35 @@ const express = require('express');
 const { json } = require('express')
 const nodemailer = require('nodemailer')
 const expressStaticGzip = require('express-static-gzip')
+const sm = require('sitemap')
 
 const app = express();
 const path = require('path')
+const sitemap = sm.createSitemap ({
+  hostname: 'https://www.nicholasmcqueen.com',
+  cacheTime: 600000,
+  urls: [
+    { url: '/',  changefreq: 'daily', priority: 1.0 },
+    { url: '/Home',  changefreq: 'daily', priority: 0.5 },
+    { url: '/About-Me',  changefreq: 'daily',  priority: 0.9 },
+    { url: '/Skills',  changefreq: 'daily', priority: 0.6 },
+    { url: '/Projects',  changefreq: 'daily', priority: 0.8 },
+    { url: '/Contact-Me',  changefreq: 'daily', priority: 0.7 },
+    { url: '/blank',  changefreq: 'daily', priority: 0.4 }
+  ]
+})
 app.use(express.static(`${__dirname}/../build`))
 app.use(`/build/client`, expressStaticGzip(`/build/client`, {
   enableBrotli: true,
   customCompressions: [{
-      encodingName: 'deflate',
-      fileExtension: 'zz'
+    encodingName: 'deflate',
+    fileExtension: 'zz'
   }],
   orderPreference: ['br', 'gz']
 }));
 app.use(json())
 
-const port = 4000 
+const port = 4000
 app.listen(port, console.log('The server is running on port', port))
 
 async function mail(req) {
@@ -30,7 +44,7 @@ async function mail(req) {
     }
   });
 
-  const output =  `
+  const output = `
   <h2>You have someone trying to contact you</h2>
   <h3>Contact Details</h3>
   <p>Name: ${req.body.name}</p>
@@ -57,10 +71,16 @@ async function mail(req) {
 app.post('/send', (req) => {
   mail(req)
 })
-app.get('/sitemap.xml', (req, res) => {
-  res.sendFile(`${__dirname}/../sitemap.xml`)
-})
+app.get('/sitemap.xml', function(req, res) {
+  sitemap.toXML( function (err, xml) {
+      if (err) {
+        return res.status(500).end();
+      }
+      res.header('Content-Type', 'application/xml');
+      res.send( xml );
+  });
+});
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
-  })
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
